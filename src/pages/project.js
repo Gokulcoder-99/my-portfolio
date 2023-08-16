4ed43e549328e1e65284be5acd12e9d483f5ccdb
Gokulcoder-99 <gokulkrishnan072@gmail.com>
2023-08-16 14:00:43 +0530

    second

--- src/pages/project.js
+++ src/pages/project.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './project.css'
-import nationalapi from '../assests/projectimage/nationalapi.png'
+import admin from '../assests/projectimage/admin.png'
 import zoro from '../assests/projectimage/zoro.png' 
 import weatherapi from '../assests/projectimage/weatherapi.png'
 import webscraper from '../assests/projectimage/webscraper.png'
@@ -47,9 +47,20 @@ function Project() {
         </div>
       </div>
       <div className='card'>
-        <img className='projectimage' src={webscraper} alt='food token generator'/>
+        <img className='projectimage' src={admin} alt='admin'/>
         <div className='detail'>
-          <h2>Webnscraper(HACKATHON)</h2>
+          <h2>AdminDashboard</h2>
+          <p>Admin Dashboard  using React, Material UI, Nivo Charts, Formik, Yup, FullCalendar, and Data Grid to build this entire application. This application consists of Light and Dark Mode, 4 different Charts, 3 different Data Table Pages, FAQ Page, Form Page, and Calendar Integration.</p>
+          <div >
+          <a target='_blank' rel='noreferrer' href='https://github.com/Gokulcoder-99/react-admin'><button className='pbtn'>FrontEnd</button></a>
+          <a target='_blank' rel='noreferrer' href="https://react-admin-e73s.vercel.app/"><button className='pbtn'>Live</button></a>
+          </div>
+        </div>
+      </div>
+      <div className='card'>
+        <img className='projectimage' src={webscraper} alt='webscraper'/>
+        <div className='detail'>
+          <h2>Webscraper(HACKATHON)</h2>
           <p>Scrap product data from E-Commerce websites.<br/>
              The scrapping of the data should be done for the following Websites.<br/>
               → Flipkart<br/>
@@ -74,18 +85,6 @@ function Project() {
           <div >
           <a target='_blank' rel='noreferrer' href='https://github.com/Gokulcoder-99/rest-countries-day-18'><button className='pbtn'>FrontEnd</button></a>
           <a target='_blank' rel='noreferrer' href="https://gokulcoder-99.github.io/rest-countries-day-18/"><button className='pbtn'>Live</button></a>
-          </div>
-        </div>
-      </div>
-      <div className='card'>
-        <img className='projectimage' src={nationalapi} alt='nationlizeapi'/>
-        <div className='detail'>
-          <h2>Nationalize API(HACKATHON)</h2>
-          <p>Implement the Nationalize API using async/await with fetch.<br/>
-             All elements are created with DOM.</p>
-          <div >
-          <a target='_blank' rel='noreferrer' href='https://github.com/Gokulcoder-99/webcode-project'><button className='pbtn'>FrontEnd</button></a>
-          <a target='_blank' rel='noreferrer' href="https://gokulcoder-99.github.io/webcode-project/"><button className='pbtn'>Live</button></a>
           </div>
         </div>
       </div>
